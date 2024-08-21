750c5db49412a9f5082ec7bb0296c9d2a6f9461a
Tasneem_Ahmed <tasneem.mostafa04@eng-st.cu.edu.eg>
2024-08-21 16:53:29 +0300

    Pop up window // close button function

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,28 @@
 // click in nav par links functionality
-let navlinks = document.querySelectorAll('.header-links li');
+let navlinks = document.querySelectorAll(".header-links li");
 navlinks.forEach((link) => {
-    link.addEventListener('click',(e) => {
-        let cursection = document.querySelector('.active');
-        console.log(e.target)
-        let sectiontoshow  = document.querySelector(`.${e.target.getAttribute('name')}`);
-        cursection.classList.remove('active');
-        sectiontoshow.classList.add('active');
-    })
-})
+  link.addEventListener("click", (e) => {
+    let cursection = document.querySelector(".active");
+    console.log(e.target);
+    let sectiontoshow = document.querySelector(
+      `.${e.target.getAttribute("name")}`
+    );
+    cursection.classList.remove("active");
+    sectiontoshow.classList.add("active");
+  });
+});
 
+const headerName = document.querySelector("header h1");
+const headerP = document.querySelector("header .info p");
+const popUp = document.querySelector(".pop-up");
+const closeBtn = document.querySelector(".pop-up .close-pop-up");
+
+const displayPopUp = function () {
+  popUp.classList.remove("hidden");
+};
+
+headerName.addEventListener("click", displayPopUp);
+headerP.addEventListener("click", displayPopUp);
+closeBtn.addEventListener("click", function () {
+  popUp.classList.add("hidden");
+});
