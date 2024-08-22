8be19fc98714740bca0c95352b398590803525ed
Hussein <shsmohmed4@gmail.com>
2024-08-22 11:05:11 +0300

    some changes & adding details page

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,16 @@
 // click in nav par links functionality
 let navlinks = document.querySelectorAll(".header-links li");
 navlinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    let cursection = document.querySelector(".active");
-    console.log(e.target);
-    let sectiontoshow = document.querySelector(
-      `.${e.target.getAttribute("name")}`
-    );
-    cursection.classList.remove("active");
-    sectiontoshow.classList.add("active");
-  });
+    link.addEventListener("click", (e) => {
+        let cursection = document.querySelector(".active");
+        let sectiontoshow = document.querySelector(
+            `.${e.target.getAttribute("name")}`
+        );
+        if (cursection) // can be removed later
+            cursection.classList.remove("active");
+        if (sectiontoshow) // can be removed later
+            sectiontoshow.classList.add("active");
+    });
 });
 
 const headerName = document.querySelector("header h1");
@@ -18,11 +19,11 @@ const popUp = document.querySelector(".pop-up");
 const closeBtn = document.querySelector(".pop-up .close-pop-up");
 
 const displayPopUp = function () {
-  popUp.classList.remove("hidden");
+    popUp.classList.remove("hidden");
 };
 
 headerName.addEventListener("click", displayPopUp);
 headerP.addEventListener("click", displayPopUp);
 closeBtn.addEventListener("click", function () {
-  popUp.classList.add("hidden");
+    popUp.classList.add("hidden");
 });
